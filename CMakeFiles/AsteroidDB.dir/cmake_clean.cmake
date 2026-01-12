351d217374cfde3c81ce5b289bc5ed3b5f62fba5
Samoreilly <x23756575@student.ncirl.ie>
2026-01-12 16:21:01 +0000

    Project structure setup

---
+++ CMakeFiles/AsteroidDB.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AsteroidDB.dir/link.d"
+  "AsteroidDB"
+  "AsteroidDB.pdb"
+  "CMakeFiles/AsteroidDB.dir/main.cpp.o"
+  "CMakeFiles/AsteroidDB.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AsteroidDB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/AsteroidDB.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AsteroidDB.
+# This may be replaced when dependencies are built.
